22e904c4fa7f7c273dabaf575b553087545f223a
narupo <narupo.glab@gmail.com>
2021-06-27 03:03:50 +0900

    fix bug: failed to read line from cd of variable

--- src/core/config.c
+++ src/core/config.c
@@ -16,12 +16,40 @@ config_new(void) {
 char *
 pop_tail_slash(char *path);
 
+static bool if_not_exists_to_mkdir(const char *dirpath) {
+    char path[FILE_NPATH];
+    if (!file_solve(path, sizeof path, dirpath)) {
+        err_error("failed to solve %s", dirpath);
+        return false;
+    }
+    if (!file_exists(path)) {
+        file_mkdirq(path);
+    }
+    return true;
+}
+
 config_t *
 config_init(config_t *self) {
     self->scope = CAP_SCOPE_LOCAL;
     self->recursion_count = 0;
 
     strcpy(self->line_encoding, "lf");
+
+    // init environment
+    if (!if_not_exists_to_mkdir("~/.cap")) {
+        return false;
+    }
+    if (!if_not_exists_to_mkdir("~/.cap/var")) {
+        return false;
+    }
+    if (!if_not_exists_to_mkdir("~/.cap/codes")) {
+        return false;
+    }
+    if (!if_not_exists_to_mkdir("~/.cap/stdlib")) {
+        return false;
+    }
+
+    // solve path
 
     if (!file_solve(self->var_cd_path, sizeof self->var_cd_path, "~/.cap/var/cd")) {
         err_error("failed to create path of cd of variable");
@@ -35,33 +63,33 @@ config_init(config_t *self) {
         err_error("failed to create path of editor of variable");
         return false;
     }
-
-    if (!file_readline(self->cd_path, sizeof self->cd_path, self->var_cd_path)) {
-        err_error("failed to read line from cd of variable");
-        return false;
-    }
-    pop_tail_slash(self->cd_path);
-
-    if (!file_readline(self->home_path, sizeof self->home_path, self->var_home_path)) {
-        err_error("failed to read line from home of variable");
-        return false;
-    }
-    pop_tail_slash(self->home_path);
-
-    if (!file_readline(self->editor, sizeof self->editor, self->var_editor_path)) {
-        err_error("failed to read line from editor of variable");
-        return false;
-    }
-
     if (!file_solve(self->codes_dir_path, sizeof self->codes_dir_path, "~/.cap/codes")) {
         err_error("failed to solve path for snippet codes directory path");
         return false;
     }
-
     // standard libraries
     if (!file_solve(self->std_lib_dir_path, sizeof self->std_lib_dir_path, "~/.cap/stdlib")) {
         err_error("failed to solve path for standard libraries directory");
         return false;
+    }
+
+    // read path from variables
+
+    if (!file_readline(self->cd_path, sizeof self->cd_path, self->var_cd_path)) {
+        // nothing todo
+    }
+    pop_tail_slash(self->cd_path);
+
+    if (!file_readline(self->home_path, sizeof self->home_path, self->var_home_path)) {
+        if (!file_solve(self->home_path, sizeof self->home_path, "~/")) {
+            err_error("failed to solve path for home path");
+            return false;
+        }
+    }
+    pop_tail_slash(self->home_path);
+
+    if (!file_readline(self->editor, sizeof self->editor, self->var_editor_path)) {
+        strcpy(self->editor, "");
     }
 
     return self;
